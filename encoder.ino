c5b7fd500521d4a851b96d5e11aab7012ab27457
Aga <agnieszkawilk8991@gmail.com>
2020-03-14 14:22:58 +0100

    A

---
+++ encoder.ino
@@ -0,0 +1,39 @@
+#define encoderPinA 2
+#define encoderPinB 4
+int encoder_count = 0;
+
+void doEncoderL() {  
+  if(digitalRead(encoderPinB)==LOW)
+  {
+    //Serial.println("prawo");
+    encoder_count++;
+  } 
+  else if (digitalRead(encoderPinB)==HIGH)
+  {
+    //Serial.println("lewo");
+    encoder_count--;
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);   //or 115200
+  
+  pinMode(encoderPinA, INPUT);
+  pinMode(encoderPinB, INPUT);
+
+  // - interrupt 0 is connected to pin 2
+  // - interrupt 1 is connected to pin 3
+  attachInterrupt(digitalPinToInterrupt(2), doEncoderL, RISING);
+  
+  digitalWrite(encoderPinA, HIGH);  //pullup resistors
+  digitalWrite(encoderPinB, HIGH);
+  Serial.println("Licznik: ");
+}
+
+
+void loop()
+{
+  Serial.println(encoder_count);
+  delay(1000);
+}
